5a3fb71b54670be27e64e946249d4b22acc7d97d
rayhanalshorif133 <rayhanalshorif@gmail.com>
2026-02-03 12:01:37 +0600

    gmail route manage

--- pages/api/gmail/fetch.js
+++ pages/api/gmail/fetch.js
@@ -1,24 +1,25 @@
 import { createClient } from '@supabase/supabase-js'
-import { NextResponse } from 'next/server'
-export default async function  handler(req, res) {
+export default async function handler(req, res) {
 
     const supabase = createClient(
         process.env.NEXT_SUPABASE_URL,
         process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY
-    )
+    );
 
     const { data, error } = await supabase
         .from('gmails')
         .select('*');
-
     if (error) {
         return res.status(500).json({ error: error.message });
     }
-    
-    return res.status(200).json({ 
+
+    return res.status(200).json({
         status: true,
         count: data.length,
         message: 'Successfully Fetch Gmails',
         data: data
-     });
+    });
+
+
+
 }
