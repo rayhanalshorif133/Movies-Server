29548634c7116717ef270b4b77e7f39d4b356d74
rayhanalshorif133 <rayhanalshorif@gmail.com>
2026-02-03 11:43:59 +0600

    gmails added

---
+++ pages/api/gmails.js
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+import { NextResponse } from 'next/server'
+export default async function  handler(req, res) {
+
+    const supabase = createClient(
+        process.env.NEXT_SUPABASE_URL,
+        process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY
+    )
+
+    const { data, error } = await supabase
+        .from('gmails')
+        .select('*');
+
+    if (error) {
+        return res.status(500).json({ error: error.message });
+    }
+    
+    return res.status(200).json({ gmails: data });
+}
