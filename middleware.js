aa28d440daf015c5b422ee360814875b54982f10
rayhanalshorif133 <rayhanalshorif@gmail.com>
2026-02-05 18:10:36 +0600

    update

--- middleware.js
+++ middleware.js
@@ -1,37 +1,11 @@
-import { createServerClient } from '@supabase/ssr'
 import { NextResponse } from 'next/server'
 
-export async function middleware(request) {
-    let response = NextResponse.next({
-        request: { headers: request.headers },
-    })
+export function middleware(request) {
+    const token = request.cookies.get('admin_token')
 
-    const supabase = createServerClient(
-        process.env.NEXT_SUPABASE_URL,
-        process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY,
-        {
-            cookies: {
-                getAll() { return request.cookies.getAll() },
-                setAll(cookiesToSet) {
-                    cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
-                    response = NextResponse.next({ request })
-                    cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options))
-                },
-            },
-        }
-    )
+    // if (!token && request.nextUrl.pathname.startsWith('/admin')) {
+    //     return NextResponse.redirect(new URL('/login', request.url))
+    // }
 
-    // Session check kora
-    const { data: { user } } = await supabase.auth.getUser()
-
-    // Jodi user logged in na thake ebong dashboard-e jete chay 
-    if (!user && request.nextUrl.pathname.startsWith('/admin/dashboard')) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
-
-    return response
-}
-
-export const config = {
-    matcher: ['/admin/:path+'],
+    return NextResponse.next();
 }
--- utils/supabase/client.js
+++ utils/supabase/client.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export function supabaseClient() {
     return createClient(
-        process.env.NEXT_SUPABASE_URL,
-        process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
     )
 }
