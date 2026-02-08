572cf62a6c78bd95f44a09d48d9d84ac8518b4f5
rayhanalshorif133 <rayhanalshorif@gmail.com>
2026-02-08 16:34:05 +0600

    added loading

--- app/page.js
+++ app/page.js
@@ -1,21 +1,22 @@
 import Header from "@/components/public/Header";
 import LoadingCard from "@/components/public/LoadingCard";
 import MovieCard from "@/components/public/MovieCard";
+import { Suspense } from 'react'
 import { createClient } from '@/utils/supabase/server'
 
 
 export default async function Home() {
   const supabase = await createClient()
 
-  // 'movies' table theke sob data ana hochche
   const { data: movies, error } = await supabase
     .from('movies')
     .select('*')
-    .order('created_at', { ascending: false }) // Newest movies age thakbe
+    .order('created_at', { ascending: false })
 
   if (error) {
     console.error('Error fetching movies:', error.message)
   }
+
   return (
     <div className="flex flex-col items-center bg-slate-900 min-h-screen justify-center font-sans">
       <Header />
@@ -23,7 +24,9 @@ export default async function Home() {
         <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
           {movies && movies.length > 0 ? (
             movies.map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
+              <Suspense key={movie.id} fallback={<LoadingCard />}>
+                <MovieCard movie={movie} />
+              </Suspense>
             ))
           ) : (
             <p className="text-white">No movies found.</p>
