58dad8e2388260392f792e35d2ae6b784817103e
rayhanalshorif133 <rayhanalshorif@gmail.com>
2026-02-08 16:04:34 +0600

    pubic header clicable done

---
+++ app/games/page.js
@@ -0,0 +1,10 @@
+import Header from "@/components/public/Header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center font-sans">
+        <Header/>
+    </div>
+  );
+}
--- components/public/Header.jsx
+++ components/public/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Header() {
     return (
@@ -7,20 +8,20 @@ export default function Header() {
                 className="bg-linear-to-r from-indigo-900 via-purple-800 to-teal-700 shadow-lg border-b border-white/10 relative z-50">
                 <div className="px-4 md:px-8 py-4 flex items-center justify-between max-w-400 mx-auto">
                     <div className="flex items-center">
-                        <a href="index.html"
+                        <Link href="index.html"
                             className="text-2xl md:text-3xl text-white font-cursive tracking-wide drop-shadow-md">
                             Entertainment Diary
-                        </a>
+                        </Link>
                     </div>
                     <div className="hidden md:flex items-center space-x-6">
-                        <a href="./games.html"
+                        <Link href="/games"
                             className="text-xs font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300 flex items-center gap-1">
                             <i className="fa-solid fa-gamepad"></i> Games
-                        </a>
-                        <a href="./index.html"
-                            className="text-xs font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Movies</a>
-                        <a href="./admin.html"
-                            className="text-xs font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Admin</a>
+                        </Link>
+                        <Link href="/movies"
+                            className="text-xs font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Movies</Link>
+                        <Link href="/login"
+                            className="text-xs font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Admin</Link>
                     </div>
                     <div className="md:hidden flex items-center">
                         <button id="mobile-menu-btn" className="text-gray-200 hover:text-white focus:outline-none">
@@ -30,13 +31,13 @@ export default function Header() {
                 </div>
                 <div id="mobile-menu" className="hidden md:hidden bg-purple-900 border-t border-white/10 shadow-xl">
                     <div className="flex flex-col px-4 py-4 space-y-4">
-                        <a href="#"
+                        <Link href="/games"
                             className="text-sm font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300"><i
-                                className="fa-solid fa-gamepad mr-2"></i>Games</a>
-                        <a href="#"
-                            className="text-sm font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Assets</a>
-                        <a href="#"
-                            className="text-sm font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Movies</a>
+                                className="fa-solid fa-gamepad mr-2"></i>Games</Link>
+                        <Link href="/movies"
+                            className="text-sm font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Movies</Link>
+                        <Link href="/login"
+                            className="text-sm font-bold text-gray-100 uppercase hover:text-yellow-300 transition duration-300">Admin</Link>
                     </div>
                 </div>
             </nav>
