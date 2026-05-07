8f8a29b90553577b7aa92a275c4672f419bc0619
rayhanalshorif133 <rayhanalshorif@gmail.com>
2026-05-07 12:51:24 +0600

    server

--- app/api/sms/route.js
+++ app/api/sms/route.js
@@ -1,3 +1,5 @@
+import { createClient } from '@/utils/supabase/server';
+import { NextResponse } from 'next/server';
 export async function GET(request) {
     const { searchParams } = new URL(request.url);
     const textMessage = searchParams.get('text');
